f2bebda4c43b5daa4614c9cbe56b91d086af9e2b
Jérémie Lumbroso <lumbroso@seas.upenn.edu>
2026-02-19 15:05:17 -0500

    chore: removed morris visualization fixings

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import MorrisCounter from "./MorrisCounter.jsx";
-
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <MorrisCounter />
-  </StrictMode>
-);
